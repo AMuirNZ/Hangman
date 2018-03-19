ea7b0b57ff784316b2cd0ec0cd67c757ca3fec62
Alisdair <Alisdair@LAPTOP-TKI3D179>
2018-03-19 14:50:53 +1300

    Update

---
+++ Hangman/GameActivity.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Hangman
+{
+    [Activity(Label = "Hangman")]
+    public class GameActivity : Activity
+    {
+        private Button A;
+        private Button B;
+        private Button C;
+        private Button D;
+        private Button E;
+        private Button F;
+        private Button G;
+        private Button H;
+        private Button I;
+        private Button J;
+        private Button K;
+        private Button L;
+        private Button M;
+        private Button N;
+        private Button O;
+        private Button P;
+        private Button Q;
+        private Button R;
+        private Button S;
+        private Button T;
+        private Button U;
+        private Button V;
+        private Button W;
+        private Button X;
+        private Button Y;
+        private Button Z;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(savedInstanceState);
+
+
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Game);
+            StartUp();
+
+
+
+        }
+
+        private void StartUp()
+        {
+            A = FindViewById<Button>(Resource.Id.btnA);
+            B = FindViewById<Button>(Resource.Id.btnB);
+            C = FindViewById<Button>(Resource.Id.btnC);
+            D = FindViewById<Button>(Resource.Id.btnD);
+            E = FindViewById<Button>(Resource.Id.btnE);
+            F = FindViewById<Button>(Resource.Id.btnF);
+            G = FindViewById<Button>(Resource.Id.btnG);
+            H = FindViewById<Button>(Resource.Id.btnH);
+            I = FindViewById<Button>(Resource.Id.btnI);
+            J = FindViewById<Button>(Resource.Id.btnJ);
+            K = FindViewById<Button>(Resource.Id.btnK);
+            L = FindViewById<Button>(Resource.Id.btnL);
+            M = FindViewById<Button>(Resource.Id.btnM);
+            N = FindViewById<Button>(Resource.Id.btnN);
+            O = FindViewById<Button>(Resource.Id.btnO);
+            P = FindViewById<Button>(Resource.Id.btnP);
+            Q = FindViewById<Button>(Resource.Id.btnQ);
+            R = FindViewById<Button>(Resource.Id.btnR);
+            S = FindViewById<Button>(Resource.Id.btnS);
+            T = FindViewById<Button>(Resource.Id.btnT);
+            U = FindViewById<Button>(Resource.Id.btnU);
+            V = FindViewById<Button>(Resource.Id.btnV);
+            W = FindViewById<Button>(Resource.Id.btnW);
+            X = FindViewById<Button>(Resource.Id.btnX);
+            Y = FindViewById<Button>(Resource.Id.btnY);
+            Z = FindViewById<Button>(Resource.Id.btnZ);
+
+
+
+            A.Click += HangmanLetter;
+            B.Click += HangmanLetter;
+            C.Click += HangmanLetter;
+            D.Click += HangmanLetter;
+            E.Click += HangmanLetter;
+            F.Click += HangmanLetter;
+            G.Click += HangmanLetter;
+            H.Click += HangmanLetter;
+            I.Click += HangmanLetter;
+            J.Click += HangmanLetter;
+            K.Click += HangmanLetter;
+            L.Click += HangmanLetter;
+            M.Click += HangmanLetter;
+            N.Click += HangmanLetter;
+            O.Click += HangmanLetter;
+            P.Click += HangmanLetter;
+            Q.Click += HangmanLetter;
+            R.Click += HangmanLetter;
+            S.Click += HangmanLetter;
+            T.Click += HangmanLetter;
+            U.Click += HangmanLetter;
+            V.Click += HangmanLetter;
+            W.Click += HangmanLetter;
+            X.Click += HangmanLetter;
+            Y.Click += HangmanLetter;
+            Z.Click += HangmanLetter;
+        }
+
+        private void HangmanLetter(object sender, EventArgs e)
+        {
+            //make a fake button
+
+            
+
+            Button fakeBtn = (Button)sender;
+
+
+            if (fakeBtn.Clickable)
+
+            {
+                fakeBtn.Enabled = false;
+
+            }
+
+            string letter = fakeBtn.Text;
+            LetterSorting(letter);
+
+            
+        }
+
+        private void LetterSorting(string letter)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            int Rnd = random.Next(1, 4);
+            String Word;
+
+            if (Rnd == 1)
+            {
+                Word = "Abolishment";
+            }
+            else if (Rnd == 2)
+            {
+                Word = "Cat";
+            }
+            else
+            {
+                Word = "Blacksmith";
+            }
+
+            
+           
+
+            string WordUC = Word.ToUpper();
+
+            //char[] letters = WordLC.ToCharArray();
+
+            Toast.MakeText(this, Word, ToastLength.Long).Show();
+
+            if (WordUC.Contains(letter))
+            {
+                Toast.MakeText(this, letter + " is in the word", ToastLength.Long).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, letter + " is not in the word", ToastLength.Long).Show();
+            }
+        }
+    }
+}
--- Hangman/MainActivity.cs
+++ Hangman/MainActivity.cs
@@ -1,18 +1,54 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
+using System;
+using Android.Content;
 
 namespace Hangman
 {
-    [Activity(Label = "Hangman", MainLauncher = true)]
+    [Activity(Label = "Hangman", MainLauncher = true, Icon =
+        "@drawable/mv")]
     public class MainActivity : Activity
     {
+        private Button btnNext;
+        private TextView txtName;
+
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
+
+
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            StartUp();
+
+        }
+
+        private void StartUp()
+        {
+            txtName = FindViewById<TextView>(Resource.Id.etEnterName);
+            btnNext = FindViewById<Button>(Resource.Id.btnNext);
+
+            btnNext.Click += onNext_Click;
+
+        }
+
+        private void onNext_Click(object sender, EventArgs e)
+        {
+            //Create an intent to move data to the other activity
+            //Toast.MakeText(this, "Hi", ToastLength.Long).Show();
+
+            var gameActivity = new Intent(this, typeof(GameActivity));
+            gameActivity.PutExtra("Name", txtName.Text);
+
+            //run the inent and start the other screen passing over the data
+            StartActivity(gameActivity);
+
         }
     }
 }
